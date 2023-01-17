e00eef52abe73304a751b59d1b856db9990ebed6
robo-bebo <106369658+robo-bebo@users.noreply.github.com>
2023-01-17 13:46:37 +0200

    Add files via upload

---
+++ Drivers/02-MCAL/01-RCC/RCC_program.c
@@ -0,0 +1,59 @@
+/*******************************************************************************************/
+/*******************************************************************************************/
+/*******************************	Author: Abanoub Ramsis	********************************/
+/*******************************	Layer: MCAL				********************************/
+/*******************************	AWC: RCC				********************************/
+/*******************************	Version: 1.0			********************************/
+/*******************************************************************************************/
+/*******************************************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "RCC_interface.h"
+#include "RCC_private.h"
+#include "RCC_config.h"
+
+void RCC_voidInitSysClock(void);
+
+
+
+void RCC_voidEnableClock(u8 Copy_u8BusId, u8 Copy_u8PerId)
+{
+	if (Copy_u8PerId <= 31)
+	{
+		switch (Copy_u8BusId)
+		{
+			case RCC_AHB  : SET_BIT(RCC_AHBENR  ,Copy_u8PerId);   break;
+			case RCC_APB1 : SET_BIT(RCC_APB1ENR ,Copy_u8PerId);   break;
+			case RCC_APB2 : SET_BIT(RCC_APB2ENR ,Copy_u8PerId);   break;
+		}
+	}
+	
+	else
+	{
+		/* Return Error */
+	}
+
+}
+
+
+void RCC_voidDisableClock(u8 Copy_u8BusId, u8 Copy_u8PerId)
+{
+	if (Copy_u8PerId <= 31)
+	{
+		switch (Copy_u8BusId)
+		{
+			case RCC_AHB  : CLR_BIT(RCC_AHBENR  ,Copy_u8PerId);   break;
+			case RCC_APB1 : CLR_BIT(RCC_APB1ENR ,Copy_u8PerId);   break;
+			case RCC_APB2 : CLR_BIT(RCC_APB2ENR ,Copy_u8PerId);   break;
+		}
+	}
+	
+	else
+	{
+		/* Return Error */
+	}
+
+}
+
