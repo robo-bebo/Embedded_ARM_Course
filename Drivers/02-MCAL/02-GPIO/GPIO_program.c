83eb836a97216fd1db12bd756e65cc2a19848ccd
robo-bebo <106369658+robo-bebo@users.noreply.github.com>
2023-01-18 20:11:01 +0200

    deleted some #defines

--- Drivers/02-MCAL/02-GPIO/GPIO_program.c
+++ Drivers/02-MCAL/02-GPIO/GPIO_program.c
@@ -7,39 +7,6 @@
 /*******************************************************************************************/
 /*******************************************************************************************/
 
-
-
-
-
-/* Options for copy_u8PortId */
-#define GPIO_PORTA	1
-#define GPIO_PORTB	2
-
-/* Options for copy_u8PinId */
-#define GPIO_PIN0		0
-#define GPIO_PIN1		1
-#define GPIO_PIN2		2
-#define GPIO_PIN3		3
-#define GPIO_PIN4		4
-#define GPIO_PIN5		5
-#define GPIO_PIN6		6
-#define GPIO_PIN7		7
-#define GPIO_PIN8		8
-#define GPIO_PIN9		9
-#define GPIO_PIN10		10
-#define GPIO_PIN11		11
-#define GPIO_PIN12		12
-#define GPIO_PIN13		13
-#define GPIO_PIN14		14
-#define GPIO_PIN15		15 
-
-
-/* Options for copy_u8PinValue */
-#define	GPIO_PIN_LOW	0
-#define	GPIO_PIN_HIGH	1
-
-
-
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 
